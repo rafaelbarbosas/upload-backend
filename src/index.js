4dbf81beb0dab3c54110a98bdf9ac17613588bcd
Rafael Barbosa <rafael.1940.b@gmail.com>
2020-02-25 16:33:15 -0300

    Add nodemon and scrips to package.json

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-app.get('/', (req,res) => {
-    return res.send('Hello world');
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.use(require('./routes'))
 
 app.listen(3000);
