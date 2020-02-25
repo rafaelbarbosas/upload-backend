bec708a2f29205f8b59f709750bff1dc73afa124
Rafael Barbosa <rafael.1940.b@gmail.com>
2020-02-25 17:00:13 -0300

    Adiciona multer - possibilita upload de arquivos para o filesystam

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 const routes = require('express').Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer');
 
-routes.get('/', (req,res) => {
-    return res.json({ hello: 'World' });
+routes.post('/posts', multer(multerConfig).single('file'), (req,res) => {
+    console.log(req.file);
+
+    return res.json({ hello: 'Rafa' });
 });
 
 module.exports = routes;
