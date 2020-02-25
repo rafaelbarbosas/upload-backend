c9c14921d22cf5c43023ce6d909f72808d3484dd
Rafael Barbosa <rafael.1940.b@gmail.com>
2020-02-25 19:07:37 -0300

    Add connection to amazon, list and delete routes

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,16 +1,30 @@
 const multer = require('multer');
 const path = require('path');
 const crypto = require('crypto');
+const aws = require('aws-sdk');
+const multerS3 = require('multer-s3');
 
-
-
-module.exports = {
-    dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-    storage: multer.diskStorage({
+const storageTypes = {
+    local: multer.diskStorage({
         destination: (req, file, cb) => {
             cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'));
         },
         filename: (req, file, cb) => {
+            crypto.randomBytes(16, (err,hash) => {
+                if(err) cb(err);
+
+                file.key = `${hash.toString('hex')}-${file.originalname}`;
+
+                cb(null, file.key);
+            });
+        }
+    }),
+    s3: multerS3({
+        s3: new aws.S3(),
+        bucket: 'upload-example-nodeapp',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: 'public-read',
+        key: (req, file, cb) => {
             crypto.randomBytes(16, (err,hash) => {
                 if(err) cb(err);
 
@@ -19,7 +33,12 @@ module.exports = {
                 cb(null, fileName);
             });
         }
-    }),
+    })
+};
+
+module.exports = {
+    dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    storage: storageTypes[process.env.STORAGE_TYPE],
     limits: {
         fileSize: 2 * 1024 * 1024
     },
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
+const path = require('path');
+
 /**
  *  Database setup
  */
-mongoose.connect('mongodb+srv://deploy:uploaddeploy@cluster0-cjk5i.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect( process.env.MONGO_URL,{
     useNewUrlParser: true
 });
 
@@ -14,6 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
 
 app.use(require('./routes'))
 
